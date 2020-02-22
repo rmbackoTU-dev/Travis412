9a08097bc44e63452924f54233d4d309a7ee2222
Ryan Backof <rmbacko92@gmail.com>
2020-02-21 21:38:10 -0500

    Added multiply and division method for exercise

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -9,4 +9,14 @@ public class SimpleCalculator {
 	public int minus(int a, int b) {
 		return a-b;
 	}
+	
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public int divide(int a, int b)
+	{
+		return a/b;
+	}
 }
