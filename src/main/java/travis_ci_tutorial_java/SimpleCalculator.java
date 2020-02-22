78ba4855ba19f4e10abea95fec07b51c47968060
Ryan Backof <rmbacko92@gmail.com>
2020-02-21 21:22:28 -0500

    Adding minus method for Exercise

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,6 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,11 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	
+	public void testMinus()
+	{
+		SimpleCalculator calc= new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2)
+	}
+	
 }
